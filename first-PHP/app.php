12334bb5f87d01533e602f03e8c426ca19cf0875
sava <savadoras@gmail.com>
2019-06-16 19:24:23 +0200

    working php

--- first-PHP/app.php
+++ first-PHP/app.php
@@ -4,18 +4,26 @@
 // zmienne predefiniowane
 $host = "127.0.0.1";
 $port = 12345;
-$wsdl 	= 'http://' . $host . ':' . $port . '?wsdl';
+$wsdl 	= 'http://' . $host . ':' . $port . '/?wsdl';
 
 // uchwyt do STDIN
 $fh = fopen('php://stdin', 'r') or die($php_errormsg);
 
 // czytanie znaków z STDIN
-while ($char = fgetc($fh)) {
-	print_r($char);
+while ($char = fgets($fh)) {
+	// print_r($char);
 
-	// $soap = new SoapClient($wsdl);
-	// $soap->getTime($line);
+	try {
+		$soap = new SoapClient($wsdl);
+		$soap->sendOver($char);
+		echo (".");
+	} catch (SoapFault $e) {
+		echo "==============================\n";
+		echo ("Błąd komunikacji z serwerem.\n");
+		echo "==============================\n\n";
+		echo $e;
+		exit();
+	}
 }
-
-// socket_close($client);
+echo ("\n");
 ?>
