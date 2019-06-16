04f928b3f7c44b39a71cc425ec20180c4af9b8c0
sava <savadoras@gmail.com>
2019-06-16 20:16:09 +0200

    Add title

--- first-PHP/app.php
+++ first-PHP/app.php
@@ -9,18 +9,18 @@ $wsdl 	= 'http://' . $host . ':' . $port . '/?wsdl';
 // uchwyt do STDIN
 $fh = fopen('php://stdin', 'r') or die($php_errormsg);
 
+echo "====================\n";
+echo "    Aplikacja #1\n";
+echo "====================\n";
+
 // czytanie znaków z STDIN
 while ($char = fgets($fh)) {
-	// print_r($char);
-
 	try {
 		$soap = new SoapClient($wsdl);
 		$soap->sendOver($char);
 		echo (".");
 	} catch (SoapFault $e) {
-		echo "==============================\n";
 		echo ("Błąd komunikacji z serwerem.\n");
-		echo "==============================\n\n";
 		echo $e;
 		exit();
 	}
